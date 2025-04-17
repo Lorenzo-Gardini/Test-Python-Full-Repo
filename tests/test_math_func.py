14ee4a82828f93f9eb0108ea7e88f2c92a854ae8
Lorenzo Gardini <l.gardo98@gmail.com>
2025-04-17 14:07:33 +0200

    Add __init__ for tests

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -2,6 +2,6 @@ import unittest
 from mylibrary.math_func import my_sum
 
 class TestMathFunc(unittest.TestCase):
-    def my_sum_test(self):
+    def test_my_sum(self):
         self.assertEqual(my_sum(2, 3), 5)
         self.assertAlmostEqual(my_sum(1.2, 3.4), 4.6)
